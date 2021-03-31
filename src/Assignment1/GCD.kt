1030f49aaa28d99df8f18db7a6b47382ca894979
user <user@Administrators-MacBook-Air.local>
2021-03-31 12:33:49 +0530

    GCD of two numbers

--- src/Assignment1/GCD.kt
+++ src/Assignment1/GCD.kt
@@ -1,4 +1,37 @@
 package Assignment1
 
-class GCD {
+// Kotlin program to find GCD of two numbers
+import java.util.*
+fun main(args: Array<String>) {
+
+    val read = Scanner(System.`in`)
+
+    var num1 = read.nextInt()
+    var num2 = read.nextInt()
+    println(gcd (num1, num2))
+
+}
+
+/**
+ * Function to calculate GCD or HCF of two numbers
+ *
+ * @param a first number
+ * @param b second number
+ * @return GCD of a and b
+ */
+public fun gcd (a: Int, b: Int): Int {
+
+    if (a == 0)
+        return b;
+    if (b == 0)
+        return a;
+
+    // base case
+    if (a == b)
+        return a;
+
+    // a is greater
+    if (a > b)
+        return gcd(a-b, b);
+    return gcd(a, b-a);
 }
---
+++ src/Assignment1/GCDKtTest.kt
@@ -0,0 +1,48 @@
+package Assignment1
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class GCDKtTest {
+
+    @Test
+    fun `gcd of 10 and 15, returns 5`() {
+        assertEquals(gcd(10,15),5)
+    }
+
+    @Test
+    fun `gcd of 15 and 10, returns 5`() {
+        assertEquals(gcd(15,10),5)
+    }
+
+    @Test
+    fun `gcd of 10 and 10, returns 5`() {
+        assertEquals(gcd(10,10),10)
+    }
+
+    @Test
+    fun `gcd of 0 and 5, returns 5`() {
+        assertEquals(gcd(0,5),5)
+    }
+
+    @Test
+    fun `gcd of 5 and 0, returns 5`() {
+        assertEquals(gcd(5,0),5)
+    }
+
+    @Test
+    fun `gcd of 1 and 5, returns 1`() {
+        assertEquals(gcd(1,5),1)
+    }
+
+    @Test
+    fun `gcd of 5 and 1, returns 1`() {
+        assertEquals(gcd(5,1),1)
+    }
+
+    @Test
+    fun `gcd of 5 and 9, returns 1`() {
+        assertEquals(gcd(5,9),1)
+    }
+
+}
